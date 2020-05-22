4c7112b0fd4daefc4f743f162b8b8a814c8cb853
demir demirel <demirdemirel@sabanciuniv.edu>
2020-05-23 01:06:54 +0300

    deleting user problem solved

--- add_delete_user_form.php
+++ add_delete_user_form.php
@@ -6,20 +6,23 @@ if (isset($_POST['ids'])){
 
 $selection_id = $_POST['ids'];
 
-$sql_statement = "DELETE FROM User WHERE Username = $selection_id";
+//$sql_statement="DELETE FROM review WHERE username = $selection_id";
 
-$sql_statement2 = "DELETE FROM User_Follows WHERE Username_followed = $selection_id";
+//$sql_statement = "DELETE FROM user WHERE username = '$selection_id'";
 
-$sql_statement3 = "DELETE FROM User_Follows WHERE Username_following = $selection_id";
+//$result = mysqli_query($db, $sql_statement);
 
-$sql_statement4 = "DELETE FROM Review WHERE Username = $selection_id";
+$sql_statement = "DELETE FROM user WHERE username = '$selection_id'";
 
-$sql_statement5 = "DELETE FROM Prefers WHERE Username = $selection_id";
+$sql_statement2 = "DELETE FROM user_follows WHERE username_followed = '$selection_id'";
 
-$sql_statement6 = "DELETE FROM Platinium_user WHERE Username = $selection_id";
+$sql_statement3 = "DELETE FROM user_follows WHERE username_following = '$selection_id'";
 
+$sql_statement4 = "DELETE FROM review WHERE username = '$selection_id'";
 
-$result = mysqli_query($db, $sql_statement);
+$sql_statement5 = "DELETE FROM prefers WHERE username = '$selection_id'";
+
+$sql_statement6 = "DELETE FROM platinium_User WHERE username = '$selection_id'";
 
 $result = mysqli_query($db, $sql_statement2);
 
@@ -31,9 +34,10 @@ $result = mysqli_query($db, $sql_statement5);
 
 $result = mysqli_query($db, $sql_statement6);
 
+$result = mysqli_query($db, $sql_statement);
 
 header ("Location: add_user.php");
-
+//echo "$selection_id";
 }
 
 else
