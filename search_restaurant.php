9653ab817add7995e6883223554d12703b04aef9
demir demirel <demirdemirel@sabanciuniv.edu>
2020-05-24 19:30:10 +0300

    add delete review item

--- search_restaurant.php
+++ search_restaurant.php
@@ -4,19 +4,21 @@ include "config.php";
 
 if (isset($_POST['rest'])){
 $selection_id = $_POST['rest'];
-
+echo "$selection_id";
 //$sql_statement="DELETE FROM review WHERE username = $selection_id";
 
 //$sql_statement = "DELETE FROM user WHERE username = '$selection_id'";
 
 //$result = mysqli_query($db, $sql_statement);
 
-$sql_statement = "SELECT * FROM Restaurant WHERE Name = '$selection_id' ";
+$sql_statement = "SELECT * FROM Restaurant WHERE Name LIKE '$selection_id' ";
 
 $result = mysqli_query($db, $sql_statement);
 
 //header ("Location: restaurant_menu.php");
-echo $result['Name'];
+while($row = mysql_fetch_array($result)) {
+    echo $row['Rest_id'];
+}
 }
 
 else
