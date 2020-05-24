aa2f20d9b3d927e73b7a031ff4e0d26bab056e6c
demir demirel <demirdemirel@sabanciuniv.edu>
2020-05-24 15:21:16 +0300

    add friend feature first attempt

--- add_friendship_form.php
+++ add_friendship_form.php
@@ -6,7 +6,7 @@ if (isset($_POST['uname'])){
 
 $Username = $_POST['uname'];
 $Username1 = $_POST['uname2'];
-echo $Username . " " . $Location . " " . $Password . "<br>";
+echo $Username . " " .  $Username1 . "<br>";
 
 $sql_statement = "INSERT INTO User_Follows(Username_followed,Username_following)
 					VALUES ('$Username','$Username1')";
